6d258408ab2c1add10ebc297c0044af60a92e7f1
Derik <derik@oagro.com.br>
2020-11-03 11:39:51 -0300

    Creating MagicBall and MagicBallstate classes and finish project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            backgroundColor: Colors.blue.shade800,
+            title: Text(
+              'Ask me Anything'
+            ),
+          ),
+          body: MagicBall(),
+        ),
       ),
     );
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int magicNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Expanded(
+        child: FlatButton(
+          onPressed: (){
+            changeRandomMagicBall();
+          },
+          child: Image.asset('images/ball$magicNumber.png'),
+        ),
+      ),
+    );
+  }
+
+  void changeRandomMagicBall() {
+    setState(() {
+      magicNumber = Random().nextInt(5) + 1;
+    });
+  }
+}
+
